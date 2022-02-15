23eb270337b285c8ac73e5cd2febfba5b32d7d35
akhilreddy21 <akhilreddy.mudumala@gmail.com>
2022-02-15 19:35:18 +0530

    added user signup auth for backend

--- server/src/index.server.js
+++ server/src/index.server.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const env = require('dotenv')
 const app = express();
+const bodyParsers = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect(
+    `mongodb+srv://Akhil:luci@cluster0.xguyy.mongodb.net/ecommerce?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+    }
+).then(() => {
+    console.log('Database Connected');
+});
+
+env.config();
+
+app.use(bodyParsers());
+app.use('/api',userRoutes);
+
+
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
